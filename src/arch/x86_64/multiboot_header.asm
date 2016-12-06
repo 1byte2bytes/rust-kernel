93c641cadd755d42c93899108a23470d1f8999aa
Sydney Erickson <sydneyerickson@icloud.com>
2016-12-06 11:31:41 -0800

    The kernel now boots to say "OK"

--- src/arch/x86_64/multiboot_header.asm
+++ src/arch/x86_64/multiboot_header.asm
@@ -1,10 +1,15 @@
 section .multiboot_header
 header_start:
-    dd 0xe85250d6                                                   ; magic number (multiboot 2 spec)
-    dd 0                                                            ; architecture bit: i386 protected mode
-    dd header_end - header_start                                    ; header length
-    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start)) ; checksum
-    ; required end tags
-    dw 0                                                            ; type
-    dw 0                                                            ; flags
-    dd 0                                                            ; size
+    dd 0xe85250d6                ; magic number (multiboot 2)
+    dd 0                         ; architecture 0 (protected mode i386)
+    dd header_end - header_start ; header length
+    ; checksum
+    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+
+    ; insert optional multiboot tags here
+
+    ; required end tag
+    dw 0    ; type
+    dw 0    ; flags
+    dd 8    ; size
+header_end:
